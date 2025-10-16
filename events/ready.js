0225634c91f69867b3eaaf53d44aabd032ebd61b
elauditorcomunista-blip <el.auditor.comunista@gmail.com>
2025-10-16 08:52:30 -0600

    Delete events directory

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'ready',
-  execute(client) {
-    console.log(`🍸 ${client.user.tag} está listo para poner música en el speakeasy.`);
-    console.log(`🥃 ${client.user.tag} está sirviendo copas en el Lackadaisy.`);
-  }
-};
