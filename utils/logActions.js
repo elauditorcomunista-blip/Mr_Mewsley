81fadff078e016169ce2acf932e98eef75eacae9
elauditorcomunista-blip <el.auditor.comunista@gmail.com>
2025-10-18 09:06:25 -0600

    Delete utils directory

--- utils/logActions.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  logAction: async (guild, message) => {
-    const canalId = '1427688193611923598'; // ID del canal de registros
-    const canalAvisos = guild.channels.cache.get(canalId);
-    if (canalAvisos) await canalAvisos.send(message);
-  }
-};
